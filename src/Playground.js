79b5d691b6013037846c72ee06988bd90e34d76f
rhivent <zzzzjrjr@gmail.com>
2019-10-28 12:03:02 +0700

    painting codes with react

---
+++ src/Playground.js
@@ -0,0 +1,6 @@
+import React, { useState, useEffect, useRef, useCallback, useMemo } from 'react'
+import randomColor from 'randomcolor'
+
+export default function Playground() {
+  return <div className="playground">Hello World!</div>
+}
